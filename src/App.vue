a9f66d31a72e6df05c15af7c2fc7f28eb0134fe6
alexandrevribeiro <vribeiro.alexandre@gmail.com>
2017-08-30 22:30:01 -0300

    Fixing initial files

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,54 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+  <div class="hello">
+    <h1>{{ message }}</h1>
+    <button @click="clicked">Click</button>
   </div>
 </template>
 
 <script lang="ts">
 export default {
-  name: 'app'
+  name: 'hello',
+  data () {
+    return {
+      message: 'Welcome to Your Vue.js App'
+    }
+  },
+
+  computed: {
+    fullMessage(){
+      return `${this.message} from Typescript`;
+    }
+  },
+
+  created() {
+    console.log('created');
+  },
+
+  methods: {
+    clicked(){
+      console.log('clicked');
+    }
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
 }
 </style>
