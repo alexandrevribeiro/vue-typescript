07b3f7a29b42279d8d1fd61ff941c74557a6f212
alexandrevribeiro <vribeiro.alexandre@gmail.com>
2017-09-02 13:33:28 -0300

    Using Two-Way model binding with "@Model" decorator to change the default event and property.

--- src/components/HelloTS.vue
+++ src/components/HelloTS.vue
@@ -2,15 +2,32 @@
     <div>
         <h1>Hello TS</h1>
         <router-link to="/">Hello</router-link>        
+        
+        <hr>
+        <my-checkbox 
+            :title="checkbox.title"
+            :value="checkbox.value" 
+            v-model="checkbox.enabled"></my-checkbox>
     </div>
 </template>
 
 <script lang="ts">
     import Vue from 'vue'
     import Component from 'vue-class-component'
+    import MyCheckbox from './MyCheckbox.vue'
     
-    @Component({})
+    @Component({
+        components: {
+            MyCheckbox
+        }
+    })
     export default class HelloTS extends Vue {
+        checkbox = {
+            title: 'Fancy checkbox',
+            value: 'checkbox-value',
+            enabled: true
+        }
+
         created() {
             console.log('HelloTS -> "created" hook.')
         }
---
+++ src/components/MyCheckbox.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="hello">
+        <input type="checkbox"
+            :value="value"
+            :checked="checked" 
+            @change="changed">
+        {{ title }}
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+    import { Component, Prop, Model } from 'vue-property-decorator'
+
+    @Component
+    export default class MyCheckbox extends Vue {
+        @Prop() title: string
+        @Prop() value: string
+
+        // Decorator to tell Vue which property and event to use
+        // In this case it's saying to use the "change" event and 
+        // the "checked" property (which is a Boolean)
+        @Prop() // -> Using "Prop" decorator since "checked" is a property
+        @Model('change') checked: boolean
+
+        changed(event) {
+            // this.$emit('input', event.target.checked)
+            this.$emit('change', event.target.checked)
+        }
+    }
+</script>
